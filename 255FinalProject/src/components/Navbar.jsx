7d117dbe201ebca3f6051ceb6ee6bbcc821d10c2
Sean Hamm <seanhamm28@gmail.com>
2024-06-01 16:02:39 -0700

    added settings and mail icons in navbar, plus additional troubleshooting

--- 255FinalProject/src/components/Navbar.jsx
+++ 255FinalProject/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
 import React from "react";
-import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "@mui/icons-material/Menu";
+import MailIcon from "@mui/icons-material/Mail";
+import SettingsIcon from "@mui/icons-material/Settings"; 
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%", // Ensure width value is a string
+}));
+
+const Icons = styled(Box)(({ theme }) => ({}));
 
 const Navbar = () => {
   return (
@@ -20,12 +38,22 @@ const Navbar = () => {
           Navigation
         </Typography>
         <Menu
-          
           sx={{
             display: { xs: "block", sm: "none" },
             "&:hover": { color: "white" },
           }}
         />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon color="action" />
+          </Badge>
+          <Badge badgeContent={4} color="secondary">
+            <SettingsIcon color="action" />
+          </Badge>
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
