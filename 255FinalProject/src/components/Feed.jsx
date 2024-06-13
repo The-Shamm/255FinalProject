305f56b8bfd21331c697de8ac0847925b27b3d8e
Sean Hamm <seanhamm28@gmail.com>
2024-06-13 08:13:05 -0700

    added feed card and some styling, but broke my create post button

--- 255FinalProject/src/components/Feed.jsx
+++ 255FinalProject/src/components/Feed.jsx
@@ -41,7 +41,79 @@ const Feed = () => {
 
   return (
     <Box bgcolor="primary.main" flex={4} p={2}>
-      <Card>
+     <Typography variant="h3" sx={{textAlign: "center", my: 2}}>Major Updates Feed</Typography>
+      
+      <Card sx={{m: 3}}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              IdSec
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="ONSR Identity and Security"
+          subheader="June 9th, 2024"
+        />
+        <CardMedia
+          component="img"
+          height="194" // Changed to a pixel value for proper display
+          image="https://images.pexels.com/photos/2743739/pexels-photo-2743739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Head Down"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Announcement, Team X is beginning work on project y, you may
+            encounter these specific issues. Please feel free to reach out for
+            additional questions, 2 hours of downtime expected
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox 
+              icon={<FavoriteBorder />} 
+              checkedIcon={<Favorite sx={{ color: 'secondary.main' }} />} // Corrected syntax for applying secondary color
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>JIRA TICKET:</Typography>
+            <Typography paragraph>Current Zoom:</Typography>
+            <Typography paragraph>
+              Summary: Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It
+              has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+      <Card sx={{m: 3}}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
