99f68a441633fe3bdbac1e97c96ae1404c1f778d
Sean Hamm <seanhamm28@gmail.com>
2024-06-12 22:01:18 -0700

    update fixed position of sidebar

--- 255FinalProject/src/App.jsx
+++ 255FinalProject/src/App.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import {
-  Box,
-  Container,
-  Typography,
-  Paper,
-  Button,
-  Stack,
-} from "@mui/material";
+import { Box, Container, Typography, Paper, Button, Stack } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
@@ -21,7 +15,7 @@ const serviceList = ["Service 1", "Service 2", "Service 3"];
 
 function App() {
   const [formData, setFormData] = useState([]);
-  const [mode, setMode] = useState("dark");
+  const [mode, setMode] = useState("light");
 
   const handleSubmit = (formDataItem) => {
     setFormData((prevData) => [...prevData, formDataItem]);
@@ -35,73 +29,81 @@ function App() {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.secondary"}>
-        <Navbar />
-        <Stack direction="row" spacing={2} justifyContent="space-between">
-          <Sidebar setMode={setMode} mode={mode}/>
-          <Feed />
-          <Rightbar />
-        </Stack>
-        <Add />
-        <Container>
-          <Typography
-            variant="h1"
-            sx={{ my: 4, textAlign: "center", color: "primary.main" }}
-          >
-            This is a test. How am I doing?
-          </Typography>
-          <Typography variant="h2">Overview</Typography>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              justifyContent: "space-between",
-              gap: 4,
-              pt: 4,
-            }}
-          >
-            {serviceList.map((service, index) => (
-              <Paper key={index} elevation={3} sx={{ width: { xs: 1, md: 320 } }}>
-                <Box sx={{ mt: 2 }}>
-                  <Typography variant="h3">{service}</Typography>
-                  <Typography sx={{ m: 2 }}>
-                    Lorem Ipsum a;lskdj f=pasdof psaokja sd;lfkj
-                    asdklofjapsdokfjas;lkdjfl;kj lkj kdjj0nmv woienpsdfoiulei
-                  </Typography>
-                  <Button
-                    variant="contained"
-                    sx={{ mt: 2, mx: 2, mb: 2, alignContent: "center" }}
-                    color="secondary"
-                  >
-                    Learn More
-                  </Button>
-                </Box>
-              </Paper>
-            ))}
-          </Box>
-          <Typography>Hello World</Typography>
-          <div className="app-container">
-            <Box sx={{ border: "2px solid grey", bgcolor: "blue", p: 1 }}>
-              <Image />
-            </Box>
-            <Typography variant="h1">
-              ***Creator notes for future feature releases***
+      <Router>
+        <Box bgcolor={"background.default"} color={"text.secondary"}>
+          <Navbar />
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+            <Sidebar setMode={setMode} mode={mode} />
+            <Routes>
+              <Route path="/" element={<Feed />} />
+              <Route 
+                path="/service-teams" 
+                element={<InputForm onSubmit={handleSubmit} />} 
+              />
+              {/* Add other routes as needed */}
+            </Routes>
+            <Rightbar />
+          </Stack>
+          <Add />
+          <Container>
+            <Typography
+              variant="h1"
+              sx={{ my: 4, textAlign: "center", color: "primary.main" }}
+            >
+              This is a test. How am I doing?
             </Typography>
-            <p>Here are some examples of links to other pages</p>
-            <nav>
-              <ul>
-                <li>
-                  <a href="./profile">Profile page test</a>
-                  <br />
-                  <a href="profile">Phonebook page</a>
-                </li>
-              </ul>
-            </nav>
-            <InputForm onSubmit={handleSubmit} />
-            <ServiceTeamTable data={formData} />
-          </div>
-        </Container>
-      </Box>
+            <Typography variant="h2">Overview</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                justifyContent: "space-between",
+                gap: 4,
+                pt: 4,
+              }}
+            >
+              {serviceList.map((service, index) => (
+                <Paper key={index} elevation={3} sx={{ width: { xs: 1, md: 320 } }}>
+                  <Box sx={{ mt: 2 }}>
+                    <Typography variant="h3">{service}</Typography>
+                    <Typography sx={{ m: 2 }}>
+                      Lorem Ipsum a;lskdj f=pasdof psaokja sd;lfkj
+                      asdklofjapsdokfjas;lkdjfl;kj lkj kdjj0nmv woienpsdfoiulei
+                    </Typography>
+                    <Button
+                      variant="contained"
+                      sx={{ mt: 2, mx: 2, mb: 2, alignContent: "center" }}
+                      color="secondary"
+                    >
+                      Learn More
+                    </Button>
+                  </Box>
+                </Paper>
+              ))}
+            </Box>
+            <Typography>Hello World</Typography>
+            <div className="app-container">
+              <Box sx={{ border: "2px solid grey", bgcolor: "blue", p: 1 }}>
+                <Image />
+              </Box>
+              <Typography variant="h1">
+                ***Creator notes for future feature releases***
+              </Typography>
+              <p>Here are some examples of links to other pages</p>
+              <nav>
+                <ul>
+                  <li>
+                    <a href="./profile">Profile page test</a>
+                    <br />
+                    <a href="profile">Phonebook page</a>
+                  </li>
+                </ul>
+              </nav>
+              <ServiceTeamTable data={formData} />
+            </div>
+          </Container>
+        </Box>
+      </Router>
     </ThemeProvider>
   );
 }
--- 255FinalProject/src/components/Sidebar.jsx
+++ 255FinalProject/src/components/Sidebar.jsx
@@ -4,74 +4,71 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import AccountBox from "@mui/icons-material/AccountBox";
 import Settings from "@mui/icons-material/Settings";
-import PersonIcon from '@mui/icons-material/Person';
-import Person from "@mui/icons-material/Person";
-import ModeNightIcon from '@mui/icons-material/ModeNight';
-
+import Person from '@mui/icons-material/Person';
+import ModeNight from '@mui/icons-material/ModeNight';
 import Add from "@mui/icons-material/Add";
-import ModeNight from "@mui/icons-material/ModeNight";
+import { Link } from "react-router-dom";
 
-const Sidebar = ({mode, setMode}) => {
+const Sidebar = ({ mode, setMode }) => {
   return (
-    <Box
+    <Box 
       bgcolor="primary.main"
       flex={1}
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      <Box position="fixed">
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#home"> 
-            <ListItemIcon>
-              <HomeIcon /> 
-            </ListItemIcon>
-            <ListItemText primary="Homepage" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list"> 
-            <ListItemIcon>
-              <Add /> 
-            </ListItemIcon>
-            <ListItemText primary="addition" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list"> 
-            <ListItemIcon>
-              <Person /> 
-            </ListItemIcon>
-            <ListItemText primary="Service Teams" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list"> 
-            <ListItemIcon>
-              <Settings /> 
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list"> 
-            <ListItemIcon>
-              <AccountBox /> 
-            </ListItemIcon>
-            <ListItemText primary="Profile" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#simple-list"> 
-            <ListItemIcon>
-              <ModeNight /> 
-            </ListItemIcon>
-            <Switch onChange={e=>setMode(mode === "light" ? "dark" : "light")} defaultChecked color="secondary" />
-          </ListItemButton>
-        </ListItem>
-      </List>
+      <Box position="sticky">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} to="/"> {/* Changed to Link for navigation */}
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} to="/addition"> {/* Changed to Link for navigation */}
+              <ListItemIcon>
+                <Add />
+              </ListItemIcon>
+              <ListItemText primary="Addition" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} to="/service-teams"> {/* Changed to Link for navigation */}
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Service Teams" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} to="/settings"> {/* Changed to Link for navigation */}
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} to="/profile"> {/* Changed to Link for navigation */}
+              <ListItemIcon>
+                <AccountBoxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch onChange={e => setMode(mode === "light" ? "dark" : "light")} defaultChecked color="secondary" />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
