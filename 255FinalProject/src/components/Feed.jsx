6820b6b1f93e31496f332f4f45fdb78ac3709089
Sean Hamm <seanhamm28@gmail.com>
2024-06-14 18:23:30 -0700

    updated typographies from lorem ipsum to actual text and added 2nd card info

--- 255FinalProject/src/components/Feed.jsx
+++ 255FinalProject/src/components/Feed.jsx
@@ -16,9 +16,9 @@ import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { styled } from "@mui/material/styles";
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
 
 // Define the ExpandMore styled component
 const ExpandMore = styled((props) => {
@@ -41,9 +41,11 @@ const Feed = () => {
 
   return (
     <Box bgcolor="primary.main" flex={4} p={2}>
-     <Typography variant="h3" sx={{textAlign: "center", my: 2}}>Major Updates Feed</Typography>
-      
-      <Card sx={{m: 3}}>
+      <Typography variant="h3" sx={{ textAlign: "center", my: 2 }}>
+        Major Updates Feed
+      </Typography>
+
+      <Card sx={{ m: 3 }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -73,9 +75,9 @@ const Feed = () => {
         </CardContent>
         <CardActions disableSpacing>
           <IconButton aria-label="add to favorites">
-            <Checkbox 
-              icon={<FavoriteBorder />} 
-              checkedIcon={<Favorite sx={{ color: 'secondary.main' }} />} // Corrected syntax for applying secondary color
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: "secondary.main" }} />} // Corrected syntax for applying secondary color
             />
           </IconButton>
           <IconButton aria-label="share">
@@ -95,25 +97,17 @@ const Feed = () => {
             <Typography paragraph>JIRA TICKET:</Typography>
             <Typography paragraph>Current Zoom:</Typography>
             <Typography paragraph>
-              Summary: Lorem Ipsum is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a
-              galley of type and scrambled it to make a type specimen book. It
-              has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Summary: Due to the scope of this work we expect service teams may
+              possible experience XYZ when part b is upgraded. This is
+              temporary.
             </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then
-              serve.
+            <Typography variant="caption">
+              For further information please contact: Doe John
             </Typography>
           </CardContent>
         </Collapse>
       </Card>
-      <Card sx={{m: 3}}>
+      <Card sx={{ m: 3 }}>
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -125,27 +119,27 @@ const Feed = () => {
               <MoreVertIcon />
             </IconButton>
           }
-          title="ONSR Identity and Security"
+          title="ONSR Change Freeze"
           subheader="June 9th, 2024"
         />
         <CardMedia
           component="img"
           height="194" // Changed to a pixel value for proper display
-          image="https://images.pexels.com/photos/2743739/pexels-photo-2743739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          image="https://images.pexels.com/photos/219837/pexels-photo-219837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Head Down"
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            Announcement, Team X is beginning work on project y, you may
-            encounter these specific issues. Please feel free to reach out for
-            additional questions, 2 hours of downtime expected
+            Announcement, The following cloud realms are currently being placed
+            under change freeze. Please do not implement any tickets unless
+            approved by SVP or higher. realms: X, Y, Z
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
           <IconButton aria-label="add to favorites">
-            <Checkbox 
-              icon={<FavoriteBorder />} 
-              checkedIcon={<Favorite sx={{ color: 'secondary.main' }} />} // Corrected syntax for applying secondary color
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: "secondary.main" }} />}
             />
           </IconButton>
           <IconButton aria-label="share">
@@ -165,20 +159,15 @@ const Feed = () => {
             <Typography paragraph>JIRA TICKET:</Typography>
             <Typography paragraph>Current Zoom:</Typography>
             <Typography paragraph>
-              Summary: Lorem Ipsum is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a
-              galley of type and scrambled it to make a type specimen book. It
-              has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Summary: Do the team X's work some tenancies are experiencing
+              problems where XYZ is unresponsive. Due to the widespread and
+              likely cause of this happening until a rollback is complete. The a
+              change freeze has been issued.
             </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then
-              serve.
+            <Typography variant="caption">
+              ONSR Change management <br></br>
+              Contact information: XXX-XXX-XXXX, <br></br>
+              Headed by: John Doe
             </Typography>
           </CardContent>
         </Collapse>
